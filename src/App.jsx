0c08b5f128ecddd314c11758bde84029dc6a107e
Devryn Zub <devryn.zub@gmail.com>
2023-10-09 00:26:51 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+
+import { logo } from './assets';
+import { Home, CreatePost } from './pages'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <header className='w-full flex-justify-between'>
+
+      </header>
+    </BrowserRouter>
+  )
+}
+
+export default App
