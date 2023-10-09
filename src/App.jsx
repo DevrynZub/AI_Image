79ca541b406576fa7793f00de0b03a03f935a69d
Devryn Zub <devryn.zub@gmail.com>
2023-10-09 01:25:44 -0600

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,14 @@ import { Home, CreatePost } from './pages'
 const App = () => {
   return (
     <BrowserRouter>
-      <header className='w-full flex-justify-between'>
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#99b6ec]">
+        <Link to="/">
+          <img src={logo} alt="logo"
+            className='w-28 object-contain' />
+        </Link>
 
+        <Link to="/create-post"
+          className="font-inter font-medium bg-[#785ae6] text-white px-4 py-2 rounded-md">Create</Link>
       </header>
     </BrowserRouter>
   )
