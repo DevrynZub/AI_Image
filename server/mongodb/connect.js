26643a4f945ff8f3f407c1d0a0ee1d0860cbd1a6
Devryn Zub <devryn.zub@gmail.com>
2023-10-11 05:06:51 -0600

    updates and started backend

---
+++ server/mongodb/connect.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+
+const connectDb = (url) => {
+  mongoose.set('strictQuery', true);
+
+  mongoose.connect(url)
+    .then(() => console.log("MongoDB connected"))
+}
