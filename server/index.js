9c36d249de3abd8346c37f7bfb053a03eff25a25
Devryn Zub <devryn.zub@gmail.com>
2023-10-11 05:59:22 -0600

    older version of openai for now

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
-import { log } from 'console';
 
 import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config();
 
@@ -11,17 +12,22 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
 app.get('/', async (req, res) => {
-  res.send('Hello from Midjourney');
-})
+  res.status(200).json({
+    message: 'Hello from DALL.E!',
+  });
+});
 
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
+    app.listen(8080, () => console.log('Server started on port 8080'));
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 startServer();
